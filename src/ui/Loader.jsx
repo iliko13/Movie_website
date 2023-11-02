ec42662c205a6b7f28edd636d73de44e91208621
iliko13 <107374131+iliko13@users.noreply.github.com>
2023-11-02 07:11:52 +0400

    commit

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute left-1/2 top-1/2 ">
+    <div className=" absolute left-1/2 top-1/2 ">
       <span className="loader"></span>
     </div>
   );
